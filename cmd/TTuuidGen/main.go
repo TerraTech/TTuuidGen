84f4779191132b47be5c1573f95b98376b8b5984
Terra <TerraTech@users.noreply.github.com>
2020-04-20 13:31:26 -0700

    Hello 2020

--- cmd/TTuuidGen/main.go
+++ cmd/TTuuidGen/main.go
@@ -1,5 +1,5 @@
 //==============================================================================
-// Copyright (c) 2019, FutureQuest, Inc.
+// Copyright (c) 2019-2020, FutureQuest, Inc.
 //   https://www.FutureQuest.net
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
